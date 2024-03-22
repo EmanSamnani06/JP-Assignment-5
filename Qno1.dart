010e62cabd341c9864197fb084c8a7683d9c6576
EmanSamnani06 <emansamnani@gmail.com>
2024-03-22 20:05:57 +0500

    Sort Method of List DataType

---
+++ Qno1.dart
@@ -0,0 +1,8 @@
+/*Write a Dart program to create a list of your favorite fruits and print them in alphabetical order.*/
+void main() {
+  List FavouriteFruits = ["Strawberry", "Apple", "Mango", "Orange"];
+
+//Sorting and printing Favourite Fruits in alphabetical order.
+  FavouriteFruits.sort();
+  print("My Favourite Fruits are: $FavouriteFruits ");
+}
